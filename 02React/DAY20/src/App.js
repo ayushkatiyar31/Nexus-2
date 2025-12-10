0f579192aef365606788e922760129b20e8dc093
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2025-12-10 22:32:37 +0530

    make changes

--- 02React/DAY20/src/App.js
+++ 02React/DAY20/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";          
 import Header from "./Components/Header.js";
+import Header from "./Components/FoodOption.js";
+import Header from "./Components/FoodCard.js";
 
 function App(){
   return (
     <>
-      <Header />
+      <Header></Header>
+      <foodOptions></foodOptions>
+      <Grocery></Grocery>
     </>
   );
 }
