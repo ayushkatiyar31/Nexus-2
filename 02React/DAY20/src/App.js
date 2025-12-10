8a801a389658e4e7904fa524b98480e104e78e2b
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2025-12-10 21:49:24 +0530

    make changes

--- 02React/DAY20/src/App.js
+++ 02React/DAY20/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
-import FoodOption from "./Components/FoodOption";
-import GroceryOption from "./Components/GroceryOption";
+ 
 
 
 
