0ddb07ee25fb19895957322b27b7b33c58c18dd8
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2025-12-10 22:41:27 +0530

    make changes

--- 02React/DAY20/src/App.js
+++ 02React/DAY20/src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";          
-import Header from "./Components/Header.js";
-import Header from "./Components/FoodOption.js";
-import Header from "./Components/FoodCard.js";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Components/Header";
+import "./index.css";  
+import FoodOption from "./Components/FoodOption";
+import GroceryOption from "./Components/GroceryOption";
+
+
+
+// Header section: Let's build it
 
 function App(){
-  return (
-    <>
-      <Header></Header>
-      <foodOptions></foodOptions>
-      <Grocery></Grocery>
-    </>
-  );
+    
+    return(
+       <>
+       <Header></Header>
+       <FoodOption></FoodOption>
+       <GroceryOption></GroceryOption>
+       </>
+    )
 }
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
+
+
