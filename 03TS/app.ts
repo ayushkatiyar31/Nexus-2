63d1c25d8426b75eb1cb581c85c9b439a5105df8
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2026-03-01 01:10:27 +0530

    leaning TypeSrcipt

---
+++ 03TS/app.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+let a: number = 10;
+let b: number = 20;
+
+let str: string = "Rohit";
+
+let isExist: boolean = true;
+isExist = false;
+
+let bignumber: bigint = 132143292183n;
+
+let abc: null = null;
+
+let bcd: undefined = undefined;
+
+let names: string = "Moahn";
+
+let honey = 20;
+
+const ho = 17;
+
+let flexibleName: string | null = "Rohit";
+flexibleName = null;
+flexibleName = "Mohan";
+
+let flexibleValue: string | number | null = "Hello";
+flexibleValue = 42;
+flexibleValue = null;
